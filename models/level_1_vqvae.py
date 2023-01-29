Fixed the issue in the gradients, apparently when doing reconstruction loss I need to cut off the embedding

models/level_1_vqvae.py
@@ -95,7 +95,7 @@ class Lvl1Decoder(nn.Module):
             
         x_out = self.end_conv(z)
             
-        return x_out
+        return F.tanh(x_out)
 
 
 class Lvl1VQ(nn.Module):
@@ -107,15 +107,16 @@ class Lvl1VQ(nn.Module):
         self.vq_codebook = VQCodebook(token_dim, num_tokens=num_tokens)
         
         
-    def forward(self, z_e, extract_losses: bool=False):
+    def forward(self, z_e: torch.Tensor, extract_losses: bool=False):
         
-        indices = self.vq_codebook.extract_indices(z_e)
-        z_q = self.vq_codebook.apply_codebook(indices).transpose(1, 2)
+        z_q, indices = self.vq_codebook.apply_codebook(z_e, code_sg=True)
         output = {'indices': indices, 'v_q': z_q}
         
         if extract_losses:
-            losses = {'alignment_loss': ((z_e.detach() - z_q) ** 2).sum(),
-                      'commitment_loss': ((z_e - z_q.detach()) ** 2).sum()}
+            emb, _ = self.vq_codebook.apply_codebook(z_e.detach())
+            output.update({'v_q_detached': emb})
+            losses = {'alignment_loss': torch.mean(torch.norm((emb - z_e.detach())**2, 2, 1)),
+                      'commitment_loss': torch.mean(torch.norm((emb.detach() - z_e)**2, 2, 1))}
             output.update(losses)
             
         return output
@@ -159,9 +160,9 @@ class Lvl1VQVariationalAutoEncoder(pl.LightningModule):
         
         # Encoder parameter initialization
         encoder_channel_list = [hidden_size, hidden_size * 2, hidden_size * 4, hidden_size * 8, hidden_size * 16, latent_depth]
-        encoder_dim_changes = [5, 5, 5, 4, 3]
+        encoder_dim_changes = [3, 4, 5, 5, 5]
         decoder_channel_list = list(reversed(encoder_channel_list))
-        decoder_dim_changes = [3, 4, 5, 5, 5]
+        decoder_dim_changes = [5, 5, 5, 4, 3]
         
         # Initialize network parts
         self.encoder = Lvl1Encoder(encoder_channel_list, encoder_dim_changes)
@@ -191,7 +192,7 @@ class Lvl1VQVariationalAutoEncoder(pl.LightningModule):
                         'output': x_out}
         
         if extract_losses:
-            total_output.update({'reconstruction_loss': F.mse_loss(x, x_out, reduction='sum')})
+            total_output.update({'reconstruction_loss': F.mse_loss(x, x_out)})
         
         return total_output
     
@@ -211,11 +212,17 @@ class Lvl1VQVariationalAutoEncoder(pl.LightningModule):
         else:
             total_steps = len(self.dataset) // self.batch_size + 1
         
-        optimizer = torch.optim.AdamW(self.parameters(), lr=self.learning_rate, weight_decay=self.weight_decay)
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)#, weight_decay=self.weight_decay)
         scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=self.learning_rate, 
                                                         epochs=self.epochs,
                                                         steps_per_epoch=total_steps)
-        return [optimizer], [scheduler]
+        # scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=1e6)
+        scheduler_settings= {'scheduler': scheduler,
+                             'interval': 'step',
+                             'monitor': 'Training reconstruction loss',
+                             'frequency': 1} # Set the scheduler to step every batch
+        
+        return [optimizer], [scheduler_settings]
     
     
     def training_step(self, batch, batch_idx):

models/vq_codebook.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import numpy as np
 
 class VQCodebook(nn.Module):
     
@@ -7,18 +8,158 @@ class VQCodebook(nn.Module):
         super().__init__()
         
         self.num_tokens = num_tokens
-        self.code_embedding = nn.Embedding(num_tokens, token_dim)
+        self.code_embedding = nn.Parameter(torch.rand(num_tokens, token_dim))
         
-    def extract_indices(self, x_in: torch.Tensor):
+    def apply_codebook(self, x_in: torch.Tensor, code_sg: bool=False):
         
-        indices_range = torch.linspace(0, self.num_tokens - 1, self.num_tokens).unsqueeze(0).repeat((x_in.shape[0], 1)).to(x_in.device)
-        embeddings = self.code_embedding(indices_range.int())
-        distances = torch.cdist(x_in.transpose(1, 2), embeddings)
-        indices = torch.argmin(distances, dim=2, keepdim=True)
-        return indices
+        embedding_weights = self.code_embedding.transpose(0, 1)
+        z_q, indices = vq_codebook_select(x_in, embedding_weights.detach() if code_sg else embedding_weights)
+        
+        return z_q, indices
+        
+        
+    # def extract_indices(self, x_in: torch.Tensor):
+        
+    #     indices_range = torch.linspace(0, self.num_tokens - 1, self.num_tokens).unsqueeze(0).repeat((x_in.shape[0], 1)).to(x_in.device)
+    #     embeddings = self.code_embedding(indices_range.int())
+    #     distances = torch.cdist(x_in.transpose(1, 2), embeddings)
+    #     indices = torch.argmin(distances, dim=2, keepdim=True)
+    #     return indices
     
-    def apply_codebook(self, indices: torch.Tensor):
-        return self.code_embedding(indices).squeeze(2)
+    # def apply_codebook(self, x_in: torch.Tensor):
+    #     indices = self.extract_indices(x_in)
+    #     return self.code_embedding(indices).squeeze(2).transpose(1, 2), indices
         
         
+
+class NearestEmbedFunc(torch.autograd.Function):
+    """
+    Helper function for deriving the the nearest embeddings, as well as passing the gradients through the embeddings themselves.
     
+    Input:
+    ------
+    x - (batch_size, emb_dim, *)
+        Last dimensions may be arbitrary
+    emb - (emb_dim, num_emb)
+    """
+    @staticmethod
+    def forward(ctx, input: torch.Tensor, emb: torch.Tensor):
+        if input.size(1) != emb.size(0):
+            raise RuntimeError('invalid argument: input.size(1) ({}) must be equal to emb.size(0) ({})'.
+                               format(input.size(1), emb.size(0)))
+
+        # save sizes for backward
+        ctx.batch_size = input.size(0)
+        ctx.num_latents = int(np.prod(np.array(input.size()[2:])))
+        ctx.emb_dim = emb.size(0)
+        ctx.num_emb = emb.size(1)
+        ctx.input_type = type(input)
+        ctx.dims = list(range(len(input.size())))
+
+        # expand to be broadcast-able
+        x_expanded = input.unsqueeze(-1)
+        num_arbitrary_dims = len(ctx.dims) - 2
+        if num_arbitrary_dims:
+            emb_expanded = emb.view(
+                emb.shape[0], *([1] * num_arbitrary_dims), emb.shape[1])
+        else:
+            emb_expanded = emb
+
+        # find nearest neighbors
+        dist = torch.norm(x_expanded - emb_expanded, 2, 1)
+        _, argmin = dist.min(-1)
+        shifted_shape = [input.shape[0], *
+                         list(input.shape[2:]), input.shape[1]]
+        result = emb.t().index_select(0, argmin.view(-1)
+                                      ).view(shifted_shape).permute(0, ctx.dims[-1], *ctx.dims[1:-1])
+
+        ctx.save_for_backward(argmin)
+        return result.contiguous(), argmin
+
+    @staticmethod
+    def backward(ctx, grad_output, argmin=None):
+        grad_input = grad_emb = None
+        if ctx.needs_input_grad[0]:
+            grad_input = grad_output
+
+        if ctx.needs_input_grad[1]:
+            argmin, = ctx.saved_variables
+            latent_indices = torch.arange(ctx.num_emb).type_as(argmin)
+            idx_choices = (argmin.view(-1, 1) ==
+                           latent_indices.view(1, -1)).type_as(grad_output.data)
+            n_idx_choice = idx_choices.sum(0)
+            n_idx_choice[n_idx_choice == 0] = 1
+            idx_avg_choices = idx_choices / n_idx_choice
+            grad_output = grad_output.permute(0, *ctx.dims[2:], 1).contiguous()
+            grad_output = grad_output.view(
+                ctx.batch_size * ctx.num_latents, ctx.emb_dim)
+            grad_emb = torch.sum(grad_output.data.view(-1, ctx.emb_dim, 1) *
+                                 idx_avg_choices.view(-1, 1, ctx.num_emb), 0)
+        return grad_input, grad_emb, None, None
+    
+
+def nearest_embed(x, emb):
+    return NearestEmbedFunc().apply(x, emb)
+
+
+class VQCodebookFunc(torch.autograd.Function):
+    
+    @staticmethod
+    def forward(ctx, x_in: torch.Tensor, embedding_weights: torch.Tensor):
+        """
+        Autograd function for the index selection. According to the VQ-VAE paper, the gradient for x_in should be a mirror, to the x_out.
+
+        Args:
+            x_in (torch.Tensor): Input, should be a BS x emb_size x codes (16 x 147)
+            embedding_weights (torch.Tensor): Embedding input tensor, should be BS x emb_size x num_codes (4 x 16 x 512)
+        """
+        
+        ctx.batch_size = x_in.shape[0]
+        
+        embedding_batch = embedding_weights.unsqueeze(0).repeat((x_in.shape[0], 1, 1))
+        x_in_t = x_in.transpose(1, 2).float()
+        embedding_batch_t = embedding_batch.transpose(1, 2).float()
+        embedding_batch_flat = embedding_batch_t.flatten(start_dim=0, end_dim=1)
+        
+        distances = torch.cdist(x_in_t, embedding_batch_t) # 4 x 147 x 512
+        indices = torch.argmin(distances, dim=2, keepdim=True) # 4 x 147 x 1
+        x_out = torch.index_select(embedding_batch_flat, dim=0, index=indices.flatten())
+        
+        x_out = x_out.view((x_in.shape[0], x_in.shape[2], x_in.shape[1]))
+        x_out = x_out.transpose(1, 2)
+        
+        ctx.save_for_backward(embedding_weights, indices)
+        
+        return x_out, indices
+        
+    @staticmethod
+    def backward(ctx, grad_outputs, indices):
+        
+        grad_input = None
+        grad_emb = None
+        
+        if ctx.needs_input_grad[0]:
+            
+            grad_input = grad_outputs
+            
+        if ctx.needs_input_grad[1]:
+            
+            embedding_weights, indices = ctx.saved_variables
+            grad_emb = torch.zeros_like(embedding_weights)
+            
+            # Feed the gradients into the grad_emb file
+            
+            for batch_idx, batch in enumerate(indices.flatten(start_dim=1)):
+                running_idx = 0
+                for idx in batch:
+                    
+                    idx_value = idx.item()
+                    
+                    grad_emb[:, idx_value] += grad_outputs[batch_idx, :, running_idx] / (indices.flatten().shape[0])
+                    running_idx += 1
+                    
+        return grad_input, grad_emb, None, None
+    
+
+def vq_codebook_select(x_in, emb_batch):
+    return VQCodebookFunc.apply(x_in, emb_batch)