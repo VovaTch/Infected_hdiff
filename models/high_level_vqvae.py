c43bb4b6e58c243d6d9767af8ce403eefd7eee7d
Vladimir Tchuiev <rmcf126@gmail.com>
2023-02-07 22:25:44 +0200

    Have now probably the best configuration I've made. If I can't train this effectively, I'll need an additional wave-unet for denoising

--- models/high_level_vqvae.py
+++ models/high_level_vqvae.py
@@ -5,6 +5,12 @@ import torch.nn as nn
 import pytorch_lightning as pl
 
 from .vq_codebook import VQCodebook
+from .level_1_vqvae import Lvl1VQ as HighLvlVQ
+import loaders
+
+
+DATASETS = {'lvl1out': loaders.Lvl1VqvaeOuts,
+            'lvl2out': loaders.Lvl2VqvaeOuts}
 
 
 class ConvBlock2D(nn.Module):
@@ -93,4 +99,70 @@ class HighLvlDecoder(nn.Module):
             
         x_out = self.end_conv(z)
             
-        return torch.tanh(x_out)
+        return x_out
+    
+    
+class HighLvlVQVariationalAutoEncoder(pl.LightningModule):
+    """
+    VQ VAE that takes a music sample and converts it into latent space, hopefully faithfully reconstructing it later.
+    This latent space is then used for the lowest level sample generation in a DiT like fashion.
+    """
+    
+    def __init__(self, 
+                 sample_rate: int,
+                 slice_length: float,
+                 hidden_size: int,
+                 latent_depth: int,
+                 learning_rate: float,
+                 weight_decay: float,
+                 batch_size: int,
+                 epochs: int,
+                 beta_factor: float=0.5,
+                 vocabulary_size: int=8192,
+                 channel_dim_change_list: List[int] = [2, 2, 2, 2, 2, 2],
+                 dataset_name: str='lvl1_out',
+                 dataset_path: str='data/vqvae_lvl1_out',
+                 optimizer_name: str='one_cycle_lr',
+                 eval_split_factor: float=0.01,
+                 **kwargs):
+        
+        super().__init__()
+        
+        # Parse arguments
+        self.cfg = kwargs
+        self.sample_rate = sample_rate
+        self.slice_length = slice_length
+        self.samples_per_slice = int(sample_rate * slice_length)
+        self.hidden_size = hidden_size
+        self.latent_depth = latent_depth
+        self.learning_rate = learning_rate
+        self.weight_decay = weight_decay
+        self.batch_size = batch_size
+        self.epochs = epochs
+        self.beta_factor = beta_factor
+        self.dataset_path = dataset_path
+        self.vocabulary_size = vocabulary_size
+        
+        # Encoder parameter initialization
+        encoder_channel_list = [hidden_size * (2 ** idx) for idx in range(len(channel_dim_change_list) + 1)]
+        encoder_channel_list[-1] = self.latent_depth
+        encoder_dim_changes = channel_dim_change_list
+        decoder_channel_list = list(reversed(encoder_channel_list))
+        decoder_dim_changes = list(reversed(channel_dim_change_list))
+        sin_locations = None
+        
+        # Initialize network parts
+        self.encoder = HighLvlEncoder(encoder_channel_list, encoder_dim_changes)
+        self.decoder = HighLvlDecoder(decoder_channel_list, decoder_dim_changes, sin_locations=sin_locations)
+        self.vq_module = HighLvlVQ(latent_depth, num_tokens=vocabulary_size)
+    
+    
+        # Datasets
+        assert dataset_name in DATASETS, f'Dataset {dataset_name} is not in the datasets options.'
+        assert 0 <= eval_split_factor <= 1, f'The split factor must be between 0 and 1, current value is {eval_split_factor}'
+        self.dataset = None
+        self.eval_split_factor = eval_split_factor
+        self.dataset_name = dataset_name
+        
+        # Optimizers
+        assert optimizer_name in ['none', 'one_cycle_lr', 'reduce_on_platou'] # TODO fix typo, program the schedulers in
