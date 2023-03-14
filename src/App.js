2d345a5a64fd2ada8ab5ffc0ab8e25d4171c90a8
Vladimir Tchuiev <rmcf126@gmail.com>
2023-03-14 22:47:24 +0200

    WIP on JS code for a web-based app, eventually will connect everything with Django

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
 
 function App() {
+
+  const [soundClip, setSoundClip] = useState([{
+    waveform_img_path: "output/img/sample_img.jpg",
+    src_path: "output/sound/sample.mp3",
+  }])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Player component placeholder
     </div>
   );
 }
---
+++ src/components/Player.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Player() {
+
+
+
+  return (
+    <div className="c-player">
+
+        <audio></audio>
+        <h4>Playing now</h4>
+        {/*controls*/}
+
+    </div>
+  )
+}
+
+export default Player;
